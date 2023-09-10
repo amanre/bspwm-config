9d42e0fe7ced869a7442859091b337f49345de43
amanre <amanre556@gmail.com>
2023-09-10 17:09:53 +0200

    update

--- etc/skel/.config/bspwm/autostart.sh
+++ etc/skel/.config/bspwm/autostart.sh
@@ -14,7 +14,7 @@ function run {
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 # set wallpaper
-wal -i usr/share/backgrounds/custom-walls/wallhaven-rrpxp1_1920x1080.jpg
+wal -i /usr/share/backgrounds/custom-walls/wallhaven-rrpxp1_1920x1080.jpg
 #sh ~/.fehbg &
 # set polybar
 $HOME/.config/bspwm/polybar/launch.sh &
