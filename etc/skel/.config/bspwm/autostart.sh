0ef7f01d2bc9024323f424bc14570cbe43f8ebec
amanre <amanre556@gmail.com>
2023-11-10 02:52:40 +0100

    update

--- etc/skel/.config/bspwm/autostart.sh
+++ etc/skel/.config/bspwm/autostart.sh
@@ -8,7 +8,7 @@ function run {
 }
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output None-1-1 --off
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
@@ -18,12 +18,10 @@ wal -i ~/.config/bspwm/walls/urban-angel-3840x2400-v0-fb4ebw5drbub1.jpg
 #sh ~/.fehbg &
 # set polybar
 $HOME/.config/bspwm/polybar/launch.sh &
-$HOME/.config/bspwm/autostart/dual-monitors-xfce.desktop &
 $HOME/.config/autostart/xscreensaver.desktop
 xsetroot -cursor_name left_ptr &
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0" &
-#$HOME/.config/bspwm/scripts/dualscreen.sh &
-#conky -c $HOME/.config/bspwm/system-overview &
+conky -c $HOME/.config/bspwm/system-overview &
 $HOME/.config/bspwm/scripts/dunst.sh &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 #run plank &
